2b31c762c44e590d66525e9a468d9de2d34ac460
qiuyang <qiuyang@qingting.fm>
2020-03-23 16:29:16 +0800

    移动零

--- app/src/main/java/com/yann/leetcode/MainActivity.kt
+++ app/src/main/java/com/yann/leetcode/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result = RemoveDuplicates().removeDuplicates(intArrayOf(0,0,1,1,1,2,2,3,3,4))
+        val result = MoveZeroes().moveZeroes(intArrayOf(0,1,0,3,12))
         Log.d("Result", "Result is $result")
 
         val myQueue = MyQueue()

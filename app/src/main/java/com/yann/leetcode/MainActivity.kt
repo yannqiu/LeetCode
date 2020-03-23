d81bcd0b55ba25ab661f91455797aec0ba4c66db
qiuyang <qiuyang@qingting.fm>
2020-03-23 15:03:50 +0800

    翻转字符串里的单词

--- app/src/main/java/com/yann/leetcode/MainActivity.kt
+++ app/src/main/java/com/yann/leetcode/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result = GetRow().getRow(33)
+        val result = ReverseWords().reverseWords("  hello world!  ")
         Log.d("Result", "Result is $result")
 
         val myQueue = MyQueue()

d7c6ca7711120f05e7093056cf80191048824cb9
qiuyang <qiuyang@qingting.fm>
2020-05-13 10:43:16 +0800

    字符串中的最长回文字串问题

---
+++ app/src/main/java/com/yann/leetcode/LongestPalindrome.kt
@@ -0,0 +1,42 @@
+package com.yann.leetcode
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+输入: "cbbd"
+输出: "bb"
+ */
+class LongestPalindrome {
+    fun longestPalindrome(s: String): String {
+        if (s.isEmpty()) return ""
+        var max = intArrayOf(0, 0)
+        s.forEachIndexed { index, c ->
+            if (index == 0 || index == s.length - 1) {
+                return@forEachIndexed
+            }
+            val jishu = maxLength(s, index, index)
+            val oushu = maxLength(s, index, index + 1)
+            if (max[1] - max[0] < jishu[1] - jishu[0]) {
+                max = jishu
+            }
+            if (max[1] - max[0] < oushu[1] - oushu[0]) {
+                max = oushu
+            }
+        }
+        return s.substring(max[0], max[1] + 1)
+    }
+
+    fun maxLength(s: String, left: Int, right: Int): IntArray {
+        var leftIndex = left
+        var rightIndex = right
+        while (s[left] == s[right] && left >= 0 && right < s.length) {
+            leftIndex--
+            rightIndex++
+        }
+        return intArrayOf(leftIndex, rightIndex)
+    }
+}
--- app/src/main/java/com/yann/leetcode/MainActivity.kt
+++ app/src/main/java/com/yann/leetcode/MainActivity.kt
@@ -9,13 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result = MoveZeroes().moveZeroes(intArrayOf(0,1,0,3,12))
+        val result = LongestPalindrome().longestPalindrome("abcdfaa")
         Log.d("Result", "Result is $result")
-
-        val myQueue = MyQueue()
-        myQueue.push(1)
-        myQueue.push(2)
-        myQueue.peek()
-
     }
 }

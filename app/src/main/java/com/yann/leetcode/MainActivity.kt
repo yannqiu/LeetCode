f34a397e3ee10792e2664b33ac40a124cbeba1af
qiuyang <qiuyang@qingting.fm>
2020-07-14 14:38:36 +0800

    有效的数独

--- app/src/main/java/com/yann/leetcode/MainActivity.kt
+++ app/src/main/java/com/yann/leetcode/MainActivity.kt
@@ -9,7 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result = GroupAnagrams().groupAnagrams(arrayListOf("eat","tea","tan","ate","nat","bat"))
+        val result = IsValidSudoku().isValidSudoku(arrayOf(
+            charArrayOf('.','.','4','.','.','.','6','3','.'),
+            charArrayOf('.','.','.','.','.','.','.','.','.'),
+            charArrayOf('5','.','.','.','.','.','.','9','.'),
+            charArrayOf('.','.','.','5','6','.','.','.','.'),
+            charArrayOf('4','.','3','.','.','.','.','.','1'),
+            charArrayOf('.','.','.','7','.','.','.','.','.'),
+            charArrayOf('.','.','.','5','.','.','.','.','.'),
+            charArrayOf('.','.','.','.','.','.','.','.','.'),
+            charArrayOf('.','.','.','.','.','.','.','.','.')
+        ))
         Log.d("Result", "Result is $result")
     }
 }

6761f28541f631af091501346e5db390ac338c82
qiuyang <qiuyang@qingting.fm>
2021-06-03 15:10:31 +0800

    把二叉搜索树转换为累加树

---
+++ app/src/main/java/com/yann/leetcode/ConvertBST.kt
@@ -0,0 +1,36 @@
+package com.yann.leetcode
+
+/**
+ *  把二叉搜索树转换为累加树
+ *  给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+
+提醒一下，二叉搜索树满足下列约束条件：
+
+节点的左子树仅包含键 小于 节点键的节点。
+节点的右子树仅包含键 大于 节点键的节点。
+左右子树也必须是二叉搜索树。
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class ConvertBST {
+    private var sum = 0
+    fun convertBST(root: TreeNode?): TreeNode? {
+        calculate(root)
+        return root
+    }
+
+    fun calculate(root: TreeNode?) {
+        if (root == null) {
+            return
+        }
+        calculate(root.right)
+        sum += root.`val`
+        root.`val` = sum
+        calculate(root.left)
+    }
+}
